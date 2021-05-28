bf33f6ecfd8b43b3f5e1895a314f080aedf8b2bf
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-28 12:07:46 +0200

    end section

--- script/main.js
+++ script/main.js
@@ -17,3 +17,39 @@ tabsContainer.addEventListener('click', (e) => {
     aboutSection.querySelector(target).classList.add('active');
   }
 });
+
+//  portfolio popup
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('btn-view-project')) {
+    togglePopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+const togglePopup = () => {
+  document.querySelector('.portfolio-popup').classList.toggle('open');
+  document.body.classList.toggle('hide-scrolling');
+  document.querySelector('.main').classList.toggle('fade-out');
+};
+
+document.querySelector('.pp-close').addEventListener('click', togglePopup);
+
+const portfolioItemDetails = (portfolioItem) => {
+  document.querySelector('.pp-thumbnail img').src = portfolioItem.querySelector(
+    '.portfolio-item-thumbnail img'
+  ).src;
+
+  document.querySelector('.pp-header h3').innerHTML =
+    portfolioItem.querySelector('.portfolio-item-title').innerHTML;
+
+  document.querySelector('.pp-body').innerHTML = portfolioItem.querySelector(
+    '.portfolio-item-details'
+  ).innerHTML;
+};
+
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('pp-inner')) {
+    togglePopup();
+  }
+});
