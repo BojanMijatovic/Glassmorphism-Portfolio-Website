c92e72c505cb3df1ebd0dcb396272c176def384f
Bojan Mijatovic <bojan.freelance.jobs@gmail.com>
2021-05-31 11:40:47 +0200

    changes

--- script/main.js
+++ script/main.js
@@ -1,23 +1,3 @@
-// tad content
-const tabsContainer = document.querySelector('.about-tabs');
-const aboutSection = document.querySelector('.about');
-
-tabsContainer.addEventListener('click', (e) => {
-  if (
-    e.target.classList.contains('btn-tab-item') &&
-    !e.target.classList.contains('active')
-  ) {
-    tabsContainer.querySelector('.active').classList.remove('active');
-    e.target.classList.add('active');
-    const target = e.target.getAttribute('data-target');
-    console.log(target);
-    aboutSection
-      .querySelector('.tab-content.active')
-      .classList.remove('active');
-    aboutSection.querySelector(target).classList.add('active');
-  }
-});
-
 //  portfolio popup
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('btn-view-project')) {
